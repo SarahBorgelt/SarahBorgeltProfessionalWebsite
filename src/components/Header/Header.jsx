c68af52ceac994387ea9deef97684eb0e70d013a
Sarah Borgelt <borgelt.sarah@gmail.com>
2026-07-10 21:39:23 -0400

    Finalized header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,12 @@
 import styles from './Header.module.css';
 import Logo from '../../assets/Logo.png';
-import HeaderHeadshot from '../../assets/HeaderHeadshot.png';
 
 export default function Header() {
 
 
      return (
           <>
-          <div className={styles.headerContainer}>
-               <img className={styles.headshot} src={HeaderHeadshot} alt="Header Headshot" />
                <img className={styles.logo} src={Logo} alt="Logo" />
-               <p className={styles.text}>
-                    Header Component
-               </p>
-          </div>
           </>
      )
 }
